2760601f351a18e13bd6b4f76fa20d7450c55ed0
Asuma Yamada <y.asuma0423@icloud.com>
2020-11-07 17:23:38 +0900

    clean up caches

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('CONSUMER_KEY', 'your consumerkey');
-define('CONSUMER_SECRET', 'your consumer secret key');
-define('ACCESS_TOKEN', 'your access token');
-define('ACCESS_TOKEN_SECRET', 'your secret access token');
