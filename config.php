7283346c4d5e0d7ee8d922a83d6a25d91093ced3
Asuma Yamada <y.asuma0423@icloud.com>
2020-11-07 20:26:30 +0900

    restore config file which is removed due to gitignore

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('CONSUMER_KEY', 'your consumerkey');
+define('CONSUMER_SECRET', 'your consumer secret key');
+define('ACCESS_TOKEN', 'your access token');
+define('ACCESS_TOKEN_SECRET', 'your secret access token');
